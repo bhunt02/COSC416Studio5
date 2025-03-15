b7ff083c784b04acc813208d6a7b29dd622eed2b
prateek-balani <prateekbalani03@gmail.com>
2025-03-15 13:57:44 -0700

    bug fix : made code less redundant

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,8 +4,8 @@ using System;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
-    public Sound[] paddleSFX, brickSFX, levelSFX; 
-    public AudioSource paddleSFXsource, brickSFXsource, levelSFXsource;
+    public Sound[] SFXsounds;
+    public AudioSource SFXsoundsource;
     private void Awake(){
         if(instance == null){
             instance = this;
@@ -14,48 +14,24 @@ public class AudioManager : MonoBehaviour
             return;
         }
         DontDestroyOnLoad(gameObject);
-        foreach(Sound s in paddleSFX){
+        foreach(Sound s in SFXsounds){
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
         }
-        foreach(Sound s in brickSFX){
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-            s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
-        }
-        foreach(Sound s in levelSFX){
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-            s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
-        }
+        
     }
 
     public void PlaySFX(string name){
-        Sound s = Array.Find(paddleSFX, sound => sound.name == name);
+        Sound s = Array.Find(SFXsounds, sound => sound.name == name);
         if(s != null){
-            paddleSFXsource.clip = s.clip;
-            paddleSFXsource.Play();
+            SFXsoundsource.clip = s.clip;
+            SFXsoundsource.Play();
             s.source.Play();
             return;
         }
-        s = Array.Find(brickSFX, sound => sound.name == name);
-        if(s != null){
-            brickSFXsource.clip = s.clip;
-            brickSFXsource.Play();
-            s.source.Play();
-            return;
-        }
-        s = Array.Find(levelSFX, sound => sound.name == name);
-        if(s != null){
-            levelSFXsource.clip = s.clip;
-            levelSFXsource.Play();
-            s.source.Play();
-            return;
-        }
+        
         Debug.LogWarning("Sound: " + name + " not found!");
     }
 }
